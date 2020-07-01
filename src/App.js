aae5bd0767345d22f194b743bf6d370c1a1ced28
Johan de Leeuw <johandeleeuw@gmail.com>
2020-07-01 22:59:10 +0200

    refactored code to use sign in with redux-sagas

--- src/App.js
+++ src/App.js
@@ -11,75 +11,67 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
+
 import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            currentUser: null,
-        };
-    }
+    this.state = {
+      currentUser: null,
+    };
+  }
 
-    unsubscribeFromAuth = null;
+  unsubscribeFromAuth = null;
 
-    componentDidMount() {
-        const { setCurrentUser } = this.props;
+  componentDidMount() {
+    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+    //     if (userAuth) {
+    //         const userRef = await createUserProfileDocument(userAuth);
+    //         userRef.onSnapshot((snapShot) => {
+    //             setCurrentUser({
+    //                 id: snapShot.id,
+    //                 ...snapShot.data(),
+    //             });
+    //         });
+    //     } else {
+    //         setCurrentUser(userAuth);
+    //     }
+    // });
+  }
 
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-            if (userAuth) {
-                const userRef = await createUserProfileDocument(userAuth);
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
 
-                userRef.onSnapshot((snapShot) => {
-                    setCurrentUser({
-                        id: snapShot.id,
-                        ...snapShot.data(),
-                    });
-                });
-            } else {
-                setCurrentUser(userAuth);
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
             }
-        });
-    }
-
-    componentWillUnmount() {
-        this.unsubscribeFromAuth();
-    }
-
-    render() {
-        return (
-            <div>
-                <Header />
-                <Switch>
-                    <Route exact path="/" component={HomePage} />
-                    <Route path="/shop" component={ShopPage} />
-                    <Route exact path="/checkout" component={CheckoutPage} />
-                    <Route
-                        exact
-                        path="/signin"
-                        render={() =>
-                            this.props.currentUser ? (
-                                <Redirect to="/" />
-                            ) : (
-                                <SignInAndSignUpPage />
-                            )
-                        }
-                    />
-                </Switch>
-            </div>
-        );
-    }
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = createStructuredSelector({
-    currentUser: selectCurrentUser,
+  currentUser: selectCurrentUser,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-    setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import { selectIsCollectingFetching } from '../../redux/shop/shop.selectors';
+import WithSpinner from '../with-spinner/with-spinner.component';
+import CollectionsOverview from './collections-overview.component';
+
+const mapStateToProps = createStructuredSelector({
+  isLoading: selectIsCollectingFetching,
+});
+
+const CollectionsOverviewContainer = compose(
+  connect(mapStateToProps),
+  WithSpinner
+)(CollectionsOverview);
+
+export default CollectionsOverviewContainer;
