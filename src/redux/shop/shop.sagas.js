8d9bc57b48ba6728d88300b7cd059d5930851b75
Johan de Leeuw <johandeleeuw@gmail.com>
2020-07-05 23:00:24 +0200

    implemtend signout into redux saga and clear cart on signout

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,35 +1,39 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import {
-  firestore,
-  convertCollectionsSnapshotToMap,
+    firestore,
+    convertCollectionsSnapshotToMap,
 } from '../../firebase/firebase.utils';
 
 import {
-  fetchCollectionsSuccess,
-  fetchCollectionsFailure,
+    fetchCollectionsSuccess,
+    fetchCollectionsFailure,
 } from './shop.actions';
 
 import ShopActionTypes from './shop.types';
 
 export function* fetchCollectionsAsync() {
-  yield console.log('I am fired');
-  try {
-    const collectionRef = firestore.collection('collections');
-    const snapshot = yield collectionRef.get();
-    const collectionsMap = yield call(
-      convertCollectionsSnapshotToMap,
-      snapshot
-    );
-    yield put(fetchCollectionsSuccess(collectionsMap));
-  } catch (error) {
-    yield put(fetchCollectionsFailure(error.message));
-  }
+    yield console.log('I am fired');
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(
+            convertCollectionsSnapshotToMap,
+            snapshot
+        );
+        yield put(fetchCollectionsSuccess(collectionsMap));
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error.message));
+    }
 }
 
 export function* fetchCollectionsStart() {
-  yield takeLatest(
-    ShopActionTypes.FETCH_COLLECTIONS_START,
-    fetchCollectionsAsync
-  );
+    yield takeLatest(
+        ShopActionTypes.FETCH_COLLECTIONS_START,
+        fetchCollectionsAsync
+    );
+}
+
+export function* shopSagas() {
+    yield all([call(fetchCollectionsStart)]);
 }
